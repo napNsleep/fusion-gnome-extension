07232aa369146834461baea428804fe9dc5643bd
napNsleep <245271671+napNsleep@users.noreply.github.com>
2026-06-24 20:52:51 -0500

    modified install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ EXTENSION_PATH="$HOME/.local/share/gnome-shell/extensions/fusion-gnome-extension
 
 mkdir -p "$EXTENSION_PATH"
 cp extension.js metadata.json "$EXTENSION_PATH"
+
+gnome-extensions enable fusion-gnome-extension@napNsleep
